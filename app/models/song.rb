a282938896fa34b25e8042c2a083e3ff6ba57192
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-05-03 16:52:32 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,8 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :release_year, presence: true
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year
+    }
 end
